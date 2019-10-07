fba171b03e7cd2958a46867facfdf4c71ee66d36
Jesse Jaggars <jjaggars@redhat.com>
2019-10-07 10:51:41 -0500

    adding promql stuff
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- go.mod
+++ go.mod
@@ -9,8 +9,10 @@ require (
 	github.com/google/btree v1.0.0 // indirect
 	github.com/googleapis/gnostic v0.3.1 // indirect
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
-	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/prometheus/client_golang v1.1.0
 	golang.org/x/crypto v0.0.0-20191002192127-34f69633bfdc // indirect
 	golang.org/x/net v0.0.0-20191003171128-d98b1b443823 // indirect
 	golang.org/x/time v0.0.0-20190921001708-c4c64cad1fd0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 
+	"github.com/prometheus/client_golang/api"
+	prom "github.com/prometheus/client_golang/api"
+	promv1 "github.com/prometheus/client_golang/api/prometheus/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -20,6 +24,16 @@ func main() {
 		panic(err.Error())
 	}
 
+	promclient, err := api.NewClient(prom.Config{
+		Address: "prometheus.mnm.svc.local:9090",
+	})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	promapi := promv1.NewAPI(promclient)
+
 	for {
 		pods, err := clientset.CoreV1().Pods("platform-prod").List(metav1.ListOptions{LabelSelector: "deploymentconfig=buck-it"})
 		if err != nil {
@@ -27,6 +41,23 @@ func main() {
 		}
 
 		fmt.Printf("There are %d pods in with the selector\n", len(pods.Items))
+
+		r := promv1.Range{
+			Start: time.Now().Add(-15 * time.Minute),
+			End:   time.Now(),
+			Step:  time.Minute,
+		}
+
+		result, warnings, err := promapi.QueryRange(context.TODO(), "sum(rate(buckit_count_total[5m]) == 0) by (kubernetes_pod_name)", r)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		if len(warnings) > 0 {
+			fmt.Printf("Warnings: %v\n", warnings)
+		}
+
+		fmt.Printf("Result:\n%v\n", result)
 
 		time.Sleep(5 * time.Second)
 	}
